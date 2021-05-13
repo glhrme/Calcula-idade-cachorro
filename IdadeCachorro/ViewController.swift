b83b923023ff81fd6d3e4d757eb1d2b3debd2e24
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-05-12 23:44:29 -0300

    Finaliza App Básico onde calcula a idade de um cachorro em anos humanos

--- IdadeCachorro/ViewController.swift
+++ IdadeCachorro/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idadeTextField: UITextField!
+    @IBOutlet weak var idadeCachorroLabel: UILabel!
+    
+    @IBAction func onClickCalcularButton(_ sender: UIButton) {
+        if let idadeCachorro = idadeTextField.text {
+            if let idadeCachorroConverted = Int(idadeCachorro) {
+                idadeCachorroLabel.text = "A idade do cachorro é: \(idadeCachorroConverted * 7)"
+                idadeCachorroLabel.isHidden = false
+            }
+
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
